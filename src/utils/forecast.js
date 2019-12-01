adfe5aeaabe7a66f2f0ed19bfb344ce2ec3d6e99
Debjani <shellysg.sengupta@gmail.com>
2019-12-01 17:15:35 +0530

    Add new data to weather report

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,17 +3,20 @@ const ftoc = require('./ftoc');
 
 const foreCast = (lat, lon, callback) => {
     const url = `https://api.darksky.net/forecast/da8ca69f3db039eb9f06060e3a895385/${lat},${lon}`;
-    
+    console.log(url)
     request({url, json:true}, (error, {body})=>{
         if(error){
             callback(error, undefined)
         } else if(body.code === 400) {
             callback("Error in latitue and longitude", undefined)
         } else {
-            let {daily,currently} = body; 
+            let {daily,currently} = body;             
             let {temperature, precipProbability} = currently;
-            let {summary} = daily
-            let data = summary+` It is currently ${ftoc(temperature)} degree centigrade out and there is ${precipProbability}% chance of rain`;
+            let {summary} = daily;
+            let {temperatureLow, temperatureHigh} = daily.data[0];
+            let data = summary+` Todays highest temperature is ${ftoc(temperatureHigh)} 
+            and lowest temperature  is ${ftoc(temperatureLow)}. 
+            It is currently ${ftoc(temperature)} degree centigrade out and there is ${precipProbability}% chance of rain`;
             callback('', data)
         }
     });
